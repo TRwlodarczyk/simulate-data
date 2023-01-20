943c64632a9b25c5a644d9b1c438c9dc012644bf
TRwlodarczyk <wlodarczyk.tr@gmail.com>
2023-01-20 10:08:31 -0700

    Initial commit

---
+++ simulate-plot-data.R
@@ -0,0 +1,10 @@
+# Simulate and plot data
+#Tomasz Wlodarczyk
+#wlodarczyk.tr@gmail.com
+
+#simulate
+x<-rnorm(n=100)
+#simulate response
+y=2*x + rnorm(n=100, sd=0.2)
+#plot the data
+plot(x=x, y=y)
